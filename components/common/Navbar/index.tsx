00d32b79104aded1844b83cb2b97dcf95e121206
ecv-rijen <rijen.maharjan@classifiedsventures.com>
2024-06-03 21:44:33 +0545

    fix: add animation to the navbar

--- components/common/Navbar/index.tsx
+++ components/common/Navbar/index.tsx
@@ -11,8 +11,13 @@ type Props = {}
 const Navbar = ({ }: Props) => {
     const pathname = usePathname();
     return (
-        <nav className="wrapper max-w-[400px] rounded-full px-12 py-6 mt-6 bg-white text-black border border-black-900/opacity-10 fixed z-10 top-0 left-0 right-0 overflow-y-auto">
-            <div className="flex justify-between items-center">
+        <motion.nav
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: [0, -10, 0], opacity: 1 }}
+            transition={{ duration: 0.6, ease: "easeInOut" }}
+            className="wrapper fixed z-10 top-0 left-0 right-0 overflow-y-auto  max-w-[400px]"
+        >
+            <div className="flex justify-between items-center rounded-full px-12 py-6 mt-6 bg-primary text-white border border-black-900/opacity-10 ">
                 <Link href={"/"}>
                     <h4 className="heading-4">{`<Rijen/>`}</h4>
                 </Link>
@@ -20,7 +25,7 @@ const Navbar = ({ }: Props) => {
                     {
                         navbarItems.map(item => (
                             <li className="uppercase text-base leading-[30px] group relative z-12" key={item.id}>
-                                <Link href={item?.link}>
+                                <Link href={item?.link} className="relative z-[1]">
                                     {item?.label}
                                 </Link>
 
@@ -28,7 +33,7 @@ const Navbar = ({ }: Props) => {
                                     initial={{ scale: 0 }}
                                     animate={pathname === item?.link ? { scale: 1 } : {}}
                                     whileHover={{ scale: 1.2, transition: { duration: 0.6 } }}
-                                    className={`absolute h-[4px] top-[13.5px] left-0 right-0 linear-gradient -z-[1] ${pathname === item?.link ? "block" : "hidden"}`}
+                                    className={`absolute h-[4px] top-[13.5px] left-0 right-0 linear-gradient group-hover:block ${pathname === item?.link ? "block" : "hidden"}`}
                                 />
                             </li>
                         ))
@@ -36,7 +41,7 @@ const Navbar = ({ }: Props) => {
                 </ul>
 
             </div>
-        </nav>
+        </motion.nav>
     )
 }
 
